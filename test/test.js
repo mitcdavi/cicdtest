185b388d3e5c092c5c37b467b8a2b69eef883f7a
mitcdavi <dmitchell@hp.com>
2016-01-26 15:52:59 +0000

    Change to expect hello world 3

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
+  it('respond with hello world 3', function(done) {
+    request(app).get('/').expect('hello world 3', done);
   });
 });
